7d818d83636ae13f78620049d1f867302b289a21
lk <kkdyx@vip.qq.com>
2017-03-08 22:58:07 +0800

    '提交日志设置'

--- src/main/java/com/lk/twentyOne/TestAction.java
+++ src/main/java/com/lk/twentyOne/TestAction.java
@@ -7,19 +7,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("/test")
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/api/test")
 @Produces("application/json")
 public class TestAction {
+	private static Logger log = LoggerFactory.getLogger(TestAction.class);
 	
 	@GET
 	public String testApi() {
-		
+		log.info("測試接口");
 		return "test helloworld!";
 	}
 	
 	@GET
 	@Path("/{param}")
 	public String helloworld(@PathParam("param") String msg) throws IOException {
+		log.info("測試參數接口，{}", msg);
 		
 		return String.format("param: %s", msg);
 	}
