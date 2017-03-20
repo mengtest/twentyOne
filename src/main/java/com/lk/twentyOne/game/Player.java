cd4db7d9a01f422dd2939b857ec94cf3b770db90
lk <kkdyx@vip.qq.com>
2017-03-20 13:16:09 +0800

    '更新player类'

--- src/main/java/com/lk/twentyOne/game/Player.java
+++ src/main/java/com/lk/twentyOne/game/Player.java
@@ -2,6 +2,8 @@ package com.lk.twentyOne.game;
 
 import java.util.List;
 
+import com.lk.twentyOne.javabean.UserInfo;
+
 /**
  * 玩家
  * @author lk
@@ -9,7 +11,51 @@ import java.util.List;
  */
 public class Player {
 	
-	private int hp = 5;
+	private int healthPoint = 5;
 	private List<Card> numberCard;
+	private List<Card> trumpCard;
+	private List<Card> efficientTrumpCard;
+	
+	private UserInfo user;
+
+	public int getHealthPoint() {
+		return healthPoint;
+	}
+
+	public void setHealthPoint(int healthPoint) {
+		this.healthPoint = healthPoint;
+	}
+
+	public List<Card> getNumberCard() {
+		return numberCard;
+	}
+
+	public void setNumberCard(List<Card> numberCard) {
+		this.numberCard = numberCard;
+	}
+
+	public List<Card> getTrumpCard() {
+		return trumpCard;
+	}
+
+	public void setTrumpCard(List<Card> trumpCard) {
+		this.trumpCard = trumpCard;
+	}
+
+	public List<Card> getEfficientTrumpCard() {
+		return efficientTrumpCard;
+	}
+
+	public void setEfficientTrumpCard(List<Card> efficientTrumpCard) {
+		this.efficientTrumpCard = efficientTrumpCard;
+	}
+
+	public UserInfo getUser() {
+		return user;
+	}
+
+	public void setUser(UserInfo user) {
+		this.user = user;
+	}
 	
 }
